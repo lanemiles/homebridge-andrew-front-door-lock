110969bda392947ef47a334fda58f7a72e505957
Lane Miles <lane.m.miles@gmail.com>
2025-01-09 02:32:18 -0800

    save

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,7 +1,8 @@
 import { Characteristic, type CharacteristicValue, type PlatformAccessory, type Service } from 'homebridge';
 // import axios from 'axios';
 // import { HttpClient } from './httpClient.js';
-import { fetchUnlockStatus, unlockDoor } from './httpClient.js'
+import axios from 'axios';
+import { fetchUnlockStatus, unlockDoor } from './httpClient.js';
 import type { AndrewFrontDoorLockHomebridgePlatform } from './platform.js';
 
 
@@ -33,12 +34,34 @@ export class AndrewFrontDoorLockHomebridgePlatformAccessory {
 
     // this.httpClient = new HttpClient('http://10.0.0.138:5000');
 
-    // setInterval(() => {
-    //   const lockState = this.getLockState();
-    //   lockState.then(state => {
-    //     this.service.updateCharacteristic(this.platform.Characteristic.LockCurrentState, state);
-    //   });
-    // }, 10000);
+    setInterval(() => {
+      console.log('DOING OUR OWN GET!');
+      const resp = axios.get('http://10.0.0.138:5000/get_status');
+      resp.then(r => {
+        if (typeof r.data !== 'object' || r.data === null) {
+          throw new Error('Response data is not an object');
+        }
+
+        console.log(`OWN | D is ${r.data}`);
+        console.log(`OWN | DU is ${r.data.unlocked}`);
+        console.log(`OWN | typeof ${typeof r.data.unlocked}`);
+        const { unlocked } = r.data.unlocked;
+        const unlockedVal = unlocked === 'true' ? true : false;
+        console.log(`retValue ${unlockedVal}`);
+
+        if (unlockedVal === true) {
+          console.log('UNLOCKED WAS TRUE');
+          this.service.updateCharacteristic(this.platform.Characteristic.LockCurrentState, this.platform.Characteristic.LockCurrentState.UNSECURED);
+          this.service.updateCharacteristic(this.platform.Characteristic.LockTargetState, this.platform.Characteristic.LockTargetState.UNSECURED);
+        } else if (unlockedVal === false) {
+          console.log('UNLOCKED WAS FALSE');
+          this.service.updateCharacteristic(this.platform.Characteristic.LockCurrentState, this.platform.Characteristic.LockCurrentState.SECURED);
+          this.service.updateCharacteristic(this.platform.Characteristic.LockTargetState, this.platform.Characteristic.LockTargetState.SECURED);
+        }
+
+        
+      });
+    }, 10000);
   
   }
 
@@ -50,14 +73,15 @@ export class AndrewFrontDoorLockHomebridgePlatformAccessory {
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
   async getLockState(): Promise<CharacteristicValue> {
+    console.log(`[${new Date().toISOString()}] NEW GET LOCK STATE`);
     try {
       const resp = await fetchUnlockStatus();
       let status = this.platform.Characteristic.LockCurrentState.UNSECURED;
       if (resp === true) {
-        console.log("UNLOCKED WAS TRUE");
+        console.log('UNLOCKED WAS TRUE');
         status = this.platform.Characteristic.LockCurrentState.UNSECURED;
       } else if (resp === false) {
-        console.log("UNLOCKED WAS FALSE");
+        console.log('UNLOCKED WAS FALSE');
         status = this.platform.Characteristic.LockCurrentState.SECURED;
       }
       console.log(`Door status: ${status}`);
@@ -78,7 +102,11 @@ export class AndrewFrontDoorLockHomebridgePlatformAccessory {
 
   async unlockDoor(value: CharacteristicValue) {
     try {
+      this.service.updateCharacteristic(this.platform.Characteristic.LockTargetState, this.platform.Characteristic.LockTargetState.UNSECURED);
       unlockDoor();
+      this.service.updateCharacteristic(this.platform.Characteristic.LockCurrentState, this.platform.Characteristic.LockCurrentState.UNSECURED);
+      this.service.updateCharacteristic(this.platform.Characteristic.LockCurrentState, this.platform.Characteristic.LockTargetState.SECURED);
+      this.service.updateCharacteristic(this.platform.Characteristic.LockTargetState, this.platform.Characteristic.LockCurrentState.SECURED);
       console.log('Door unlocked');
     } catch (error) {
       if (error instanceof Error) {
